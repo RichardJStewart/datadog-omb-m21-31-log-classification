7a650e38ebbd678179a0c67b565c4a54b965b20b
Rick Stewart <153671569+RichardJStewart@users.noreply.github.com>
2026-03-02 11:38:26 -0500

    Added metrics creation command file
    This file has the API commands to create the metrics used to count logs per EL tier

---
+++ omb-m2131-metrics.cmd
@@ -0,0 +1,61 @@
+curl -X POST "https://api.datadoghq.com/api/v2/logs/config/metrics" \
+  -H "DD-API-KEY: <YOUR_API_KEY>" \
+  -H "DD-APPLICATION-KEY: <YOUR_APP_KEY>" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "data": {
+      "type": "logs_metrics",
+      "attributes": {
+        "name": "omb.m2131.el0.count",
+        "filter": { "query": "omb.m2131_el:el0" },
+        "compute": { "aggregation_type": "count" }
+      }
+    }
+  }'
+
+curl -X POST "https://api.datadoghq.com/api/v2/logs/config/metrics" \
+  -H "DD-API-KEY: <YOUR_API_KEY>" \
+  -H "DD-APPLICATION-KEY: <YOUR_APP_KEY>" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "data": {
+      "type": "logs_metrics",
+      "attributes": {
+        "name": "omb.m2131.el1.count",
+        "filter": { "query": "omb.m2131_el:el1" },
+        "compute": { "aggregation_type": "count" }
+      }
+    }
+  }'
+
+curl -X POST "https://api.datadoghq.com/api/v2/logs/config/metrics" \
+  -H "DD-API-KEY: <YOUR_API_KEY>" \
+  -H "DD-APPLICATION-KEY: <YOUR_APP_KEY>" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "data": {
+      "type": "logs_metrics",
+      "attributes": {
+        "name": "omb.m2131.el2.count",
+        "filter": { "query": "omb.m2131_el:el2" },
+        "compute": { "aggregation_type": "count" }
+      }
+    }
+  }'
+
+curl -X POST "https://api.datadoghq.com/api/v2/logs/config/metrics" \
+  -H "DD-API-KEY: <YOUR_API_KEY>" \
+  -H "DD-APPLICATION-KEY: <YOUR_APP_KEY>" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "data": {
+      "type": "logs_metrics",
+      "attributes": {
+        "name": "omb.m2131.el3.count",
+        "filter": { "query": "omb.m2131_el:el3" },
+        "compute": { "aggregation_type": "count" }
+      }
+    }
+  }'
+
+
