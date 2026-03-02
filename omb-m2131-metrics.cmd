f00009f5232db7db84be1ea6cf4278569c6d5bf4
Rick Stewart <153671569+RichardJStewart@users.noreply.github.com>
2026-03-02 16:34:56 -0500

    Update omb-m2131-metrics.cmd
    Removed ".count" to the metric names.

--- omb-m2131-metrics.cmd
+++ omb-m2131-metrics.cmd
@@ -6,7 +6,7 @@ curl -X POST "https://api.datadoghq.com/api/v2/logs/config/metrics" \
     "data": {
       "type": "logs_metrics",
       "attributes": {
-        "name": "omb.m2131.el0.count",
+        "name": "omb.m2131.el0",
         "filter": { "query": "omb.m2131_el:el0" },
         "compute": { "aggregation_type": "count" }
       }
@@ -21,7 +21,7 @@ curl -X POST "https://api.datadoghq.com/api/v2/logs/config/metrics" \
     "data": {
       "type": "logs_metrics",
       "attributes": {
-        "name": "omb.m2131.el1.count",
+        "name": "omb.m2131.el1",
         "filter": { "query": "omb.m2131_el:el1" },
         "compute": { "aggregation_type": "count" }
       }
@@ -36,7 +36,7 @@ curl -X POST "https://api.datadoghq.com/api/v2/logs/config/metrics" \
     "data": {
       "type": "logs_metrics",
       "attributes": {
-        "name": "omb.m2131.el2.count",
+        "name": "omb.m2131.el2",
         "filter": { "query": "omb.m2131_el:el2" },
         "compute": { "aggregation_type": "count" }
       }
@@ -51,7 +51,7 @@ curl -X POST "https://api.datadoghq.com/api/v2/logs/config/metrics" \
     "data": {
       "type": "logs_metrics",
       "attributes": {
-        "name": "omb.m2131.el3.count",
+        "name": "omb.m2131.el3",
         "filter": { "query": "omb.m2131_el:el3" },
         "compute": { "aggregation_type": "count" }
       }
