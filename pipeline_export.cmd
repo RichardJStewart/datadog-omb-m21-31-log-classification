b6c4f0ec090a92abc562cbcec4cb6dcf0507144d
Rick Stewart <153671569+RichardJStewart@users.noreply.github.com>
2026-03-02 11:13:29 -0500

    Pipeline and Reference Table files initial upload
    These files form the basis of the OMB M21-31 support within Datadog.

---
+++ pipeline_export.cmd
@@ -0,0 +1,5 @@
+curl -X GET "https://api.datadoghq.com/api/v1/logs/config/pipelines/OGSM7Pj4TTSfroeqVP3tRQ" \
+  -H "DD-API-KEY: $DD_API_KEY" \
+  -H "DD-APPLICATION-KEY: $DD_APP_KEY" \
+-H "Content-Type: application/json" \
+-o omb_m2131_pipeline.json
