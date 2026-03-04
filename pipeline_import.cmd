20795dba40d0248fe80bec9ad80b85ad74a1419d
Rick Stewart <153671569+RichardJStewart@users.noreply.github.com>
2026-03-04 09:51:48 -0500

    pipeline import
    adding import pipeline command file example

---
+++ pipeline_import.cmd
@@ -0,0 +1,5 @@
+curl -X POST "https://api.datadoghq.com/api/v1/logs/config/pipelines" \
+  -H "Content-Type: application/json" \
+  -H "DD-API-KEY: <YOUR_API_KEY>" \
+  -H "DD-APPLICATION-KEY: <YOUR_APP_KEY>" \
+  -d @omb_m2131_pipeline.json
