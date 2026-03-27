3fda54d6f1706e6539831ee01c842275b473d563
RichardJStewart <richard.stewart@datadoghq.com>
2026-03-27 12:41:51 -0400

    modifying import command to refelct pipeline json file location

--- pipeline_import.cmd
+++ pipeline_import.cmd
@@ -2,4 +2,4 @@ curl -X POST "https://api.datadoghq.com/api/v1/logs/config/pipelines" \
   -H "Content-Type: application/json" \
   -H "DD-API-KEY: <YOUR_API_KEY>" \
   -H "DD-APPLICATION-KEY: <YOUR_APP_KEY>" \
-  -d @omb_m2131_pipeline.json
+  -d @pipelines/omb_m2131_pipeline.json
